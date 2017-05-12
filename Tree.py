acb896a4aa7b382c045791e9763c403af82321ea
edoardore@me.com <dbv182626>
2017-05-12 16:53:41 +0200

    1.0

---
+++ Tree.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.dad = None
+
+    def get(self):
+        return self.key
+
+    def set(self, key):
+        self.key = key
+
+    def getChildren(self):
+        children = []
+        if (self.left != None):
+            children.append(self.left)
+        if (self.right != None):
+            children.append(self.right)
+
+    def getDad(self):
+        return self.dad
+
+    def setDad(self, dad):
+        self.dad = dad
+
+
+class ABR:
+    def __init__(self):
+        self.root = None
+
+    def setRoot(self, key):
+        self.root = Node(key)
+
+    def insert(self, key):
+        if (self.root is None):
+            self.setRoot(key)
+        else:
+            self.insertNode(self.root, key)
+
+    def insertNode(self, currentNode, key):
+        if (key <= currentNode.key):
+            if (currentNode.left):
+                self.insertNode(currentNode.left, key)
+            else:
+                currentNode.left = Node(key)
+        elif (key > currentNode.key):
+            if (currentNode.right):
+                self.insertNode(currentNode.right, key)
+            else:
+                currentNode.right = Node(key)
+
+    def find(self, key):
+        return self.findNode(self.root, key)
+
+    def findNode(self, currentNode, key):
+        if (currentNode is None):
+            return False
+        elif (key == currentNode.key):
+            return True
+        elif (key < currentNode.key):
+            return self.findNode(currentNode.left, key)
+        else:
+            return self.findNode(currentNode.right, key)
+
+    def inorder(self):
+        def _inorder(v):
+            if (v is None):
+                return
+            if (v.left is not None):
+                _inorder(v.left)
+            print v.key
+            if (v.right is not None):
+                _inorder(v.right)
+
+        _inorder(self.root)
+
+    def trapianto(self, currentNode, key):
+
+    def delete(self, key):
+
+
+tree = ABR()
+tree.insert(4)
+tree.insert(5)
+for x in range(20, 10, -1):
+    tree.insert(x)
+print tree.find(5)
+print tree.find(2)
+tree.inorder()
